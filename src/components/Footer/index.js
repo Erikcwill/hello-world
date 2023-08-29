6242b77803a9e13d43affb61bfd4049b1268f747
Erick William <erikcwill@gmail.com>
2023-08-29 17:30:56 -0300

    General Update

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+import { ReactComponent as Trademark } from "assets/trademark.svg";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <Trademark />
+    </footer>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutMe from "./pages/AboutMe";
 import Home from "./pages/Home";
 import Menu from "./components/Menus";
+import Footer from "components/Footer";
 
 function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ function AppRoutes() {
         <Route path="/aboutme" element={<AboutMe />} />
         <Route path="*" element={<div>404</div>} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
